328cecd23b3ed23dfafa1dfeda64ad414dbf659c
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-08-04 14:46:51 -0400

    Got everything ready for deployment to Heroku.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7=85ky&zmwyviqqflfck6eh7pq&_h%e9eqv_7b+q8=+8x@^*rx'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,11 +78,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://ktgjtvdpbtdngt:56a8f7e3a871450ab8316f82bed5666d3cd3c65ff71c76fe17c8fda747a00874@ec2-34-194-130-103.compute-1.amazonaws.com:5432/dc40cma2pclrvt')
 }
 
 
